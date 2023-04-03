1df2e149b0e33096e450eed92416d061c05336c7
radnotizsuzsanna <radnotizsuzsanna42@gmail.com>
2023-04-03 23:00:30 +0200

    tachograf 6

--- train-interfaces/src/main/java/hu/bme/mit/train/interfaces/TrainSensor.java
+++ train-interfaces/src/main/java/hu/bme/mit/train/interfaces/TrainSensor.java
@@ -1,9 +1,14 @@
 package hu.bme.mit.train.interfaces;
 
+import java.time.LocalDateTime;
+
 public interface TrainSensor {
 
 	int getSpeedLimit();
 
 	void overrideSpeedLimit(int speedLimit);
 
+	void record();
+
+	Integer getTachograf(LocalDateTime time, Integer joystickPosition );
 }
--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -1,14 +1,21 @@
 package hu.bme.mit.train.sensor;
 
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainSensor;
 import hu.bme.mit.train.interfaces.TrainUser;
+
+import java.time.LocalDateTime;
 
 public class TrainSensorImpl implements TrainSensor {
 
 	private TrainController controller;
 	private TrainUser user;
 	private int speedLimit = 5;
+
+	Table<LocalDateTime, Integer, Integer> tachograf = HashBasedTable.create();
+
 
 	public TrainSensorImpl(TrainController controller, TrainUser user) {
 		this.controller = controller;
@@ -24,6 +31,16 @@ public class TrainSensorImpl implements TrainSensor {
 	public void overrideSpeedLimit(int speedLimit) {
 		this.speedLimit = speedLimit;
 		controller.setSpeedLimit(speedLimit);
+	}
+
+	@Override
+	public void record() {
+		tachograf.put(LocalDateTime.now(), user.getJoystickPosition(), controller.getReferenceSpeed());
+	}
+
+	@Override
+	public Integer getTachograf(LocalDateTime time, Integer joystickPosition) {
+		return tachograf.get(time, joystickPosition);
 	}
 
 }

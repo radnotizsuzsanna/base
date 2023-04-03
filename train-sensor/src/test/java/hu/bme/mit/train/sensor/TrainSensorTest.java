c5a40585b25bd7cee60df6760c696955d3620c6e
radnotizsuzsanna <radnotizsuzsanna42@gmail.com>
2023-04-03 22:16:41 +0200

    tachograf 2

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -5,6 +5,8 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
+import java.time.LocalDateTime;    
+
 public class TrainSensorTest {
 
 	TrainController controller;
